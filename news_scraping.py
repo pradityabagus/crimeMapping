4f9f51f11ff01c473778176fdf8a76026d3a8560
pradityabagus <praditya@ehealth.co.id>
2020-08-19 22:10:32 +0700

    fixes

--- news_scraping.py
+++ news_scraping.py
@@ -10,25 +10,40 @@ ctx.verify_mode = ssl.CERT_NONE
 # scraping parameters
 startyear = 2020
 endyear = 2020
-
 startmonth = 7
 endmonth = 7
-
 startday = 1
-endday = 1
-
+endday = 31
 page = 1
 
 for year in range(startyear,endyear+1):
     for month in range(startmonth,endmonth+1):
         for day in range(startday,endday+1):
-            req = urllib.request.Request('https://www.jpnn.com/indeks?id=276&d='+str(day)+'&m='+str(month)+'&y='+str(year)+'&tab=all&page='+str(page), headers={'User-Agent': 'Mozilla/5.0'})
+            page = 1
+            while True:
+                req = urllib.request.Request('https://www.jpnn.com/indeks?id=276&d='+str(day)+'&m='+str(month)+'&y='+str(year)+'&tab=all&page='+str(page), headers={'User-Agent': 'Mozilla/5.0'})
 
-            data = urllib.request.urlopen(req, context=ctx).read().decode()
-            bsData = bs(data, 'html.parser')
+                data = urllib.request.urlopen(req, context=ctx).read().decode()
+                bsData = bs(data, 'html.parser')
 
-            links = bsData.select('#content-utama > div.kolom-kiri > div.border-box > div > ul > li > div.content-description > div > h2 > a')
+                linkSelector = '#content-utama > div.kolom-kiri > div.border-box > div > ul > li > div.content-description > div > h2 > a'
 
-            for link in links:
-                print(link['href'])
-            
+                links = bsData.select(linkSelector)
+
+                print('--------')
+                print('Parsing page'+str(page))
+                print('--------')
+
+                for link in links:
+                    title = link.contents[0]
+                    print(str(day)+'/'+str(month)+'/'+str(year))
+                    print(str(title))
+                    print(link['href'])
+                    print("-")
+
+                nextButton = bsData.find_all(rel='next')
+                
+                if nextButton == []:
+                    break
+
+                page += 1
